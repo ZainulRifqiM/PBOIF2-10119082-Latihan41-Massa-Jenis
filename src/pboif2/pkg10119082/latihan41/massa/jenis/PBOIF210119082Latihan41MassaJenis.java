5f4a6bf613053d8aba39930c5ae6619a4d14a7d9
Zainul Rifqi <Zainul Rifqi@LAPTOP-O1E2M14H>
2020-11-13 10:01:39 +0700

    PBOIF2-10119082-Latihan41-Massa Jenis

---
+++ src/pboif2/pkg10119082/latihan41/massa/jenis/PBOIF210119082Latihan41MassaJenis.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119082.latihan41.massa.jenis;
+
+/**
+ *
+ * @author Zainul Rifqi
+ * NAMA     : Zainul Rifqi Muwaffaq
+ * KELAS    : IF2
+ * NIM      : 10119082
+ * Deskripsi: Massa Jenis
+ */
+public class PBOIF210119082Latihan41MassaJenis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Kubus kubus = new Kubus();
+        
+        System.out.println("=====Massa Jenis Kubus=====");
+        kubus.setSisi(5);
+        System.out.println("Sisi : " + kubus.getSisi());
+        kubus.setMassa(250);
+        System.out.println("Massa : " + kubus.getMassa());
+        
+        System.out.println();
+        System.out.println("=====Hasil Perhitungan=====");
+        System.out.println("Volume : " + kubus.hitungVolume(kubus.getSisi()));
+        System.out.println("Massa Jenis : " + kubus.hitungMassaJenis(kubus.getMassa(),kubus.hitungVolume(kubus.getSisi())));
+    }
+    
+}
